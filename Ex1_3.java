b4fd9bf2d477110b69bc46f20c0b75028511390e
Jordan <jshape32@gmail.com>
2020-02-11 10:19:37 -0700

    added chapter 1 to folder

--- Ex1_3.java
+++
@@ -1,9 +0,0 @@
-/*Jordan Shakespeare 2-10-2020 this code runs division*/
-public class Ex1_3{
-    public static void main(String[]args){
-
-
-        double Result=(9.5*4.5-2.5*3)/(45.5-3.5);
-               System.out.println(Result);
-    }
-}

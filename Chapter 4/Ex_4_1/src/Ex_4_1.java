9a6b7ac09a9f865c73bdbf501588d3840a184d57
jshakens <60676397+jshakens@users.noreply.github.com>
2020-02-27 11:09:59 -0700

    Add files via upload

---
+++ Chapter 4/Ex_4_1/src/Ex_4_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+/* Jordan Shakespeare 2-24-2020 Code computes area of pentagon*/
+public class Ex_4_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("R = ");
+        double r = scanner.nextDouble();
+        double s = (2 * r * Math.sin(Math.PI / 5));
+        double area = (5* Math.pow(s, 2))/(4*(Math.tan(Math.PI/5)));
+        System.out.println("Area = "+ area);
+    }
+}
